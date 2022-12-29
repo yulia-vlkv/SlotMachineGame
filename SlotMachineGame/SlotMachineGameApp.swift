a4856827fa8695937ece1259890ee12511dc0995
Iuliia Vlkv <yu.wlkw@gmail.com>
2022-12-29 15:17:23 +0500

    Added assets

---
+++ SlotMachineGame/SlotMachineGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineGameApp.swift
+//  SlotMachineGame
+//
+//  Created by Iuliia Volkova on 29.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SlotMachineGame/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SlotMachineGame
+//
+//  Created by Iuliia Volkova on 29.12.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
